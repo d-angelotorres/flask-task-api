2da1e7ff098c1f6a17243c7081ed9d1e8c5ea0ca
d-angelotorres <dangelotorres2@gmail.com>
2025-04-17 13:22:32 -0400

    Add root route and use it for CI health check ✅

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def delete_task(task_id):
 
 if __name__ == '__main__':
     logger.info("🚀 Starting Flask server on http://0.0.0.0:5050")
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=False, host='0.0.0.0', port=5050)
